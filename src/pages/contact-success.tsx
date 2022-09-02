c69b1da1309dfb632f54250c71b6495034464498
schebet <91207140+schebet@users.noreply.github.com>
2022-09-02 13:28:47 +0200

    Update contact-success.tsx

--- src/pages/contact-success.tsx
+++ src/pages/contact-success.tsx
@@ -4,9 +4,9 @@ import { Layout } from "../components/layout";
 export const ContactSuccess: FunctionComponent = () => {
   return (
     <Layout>
-      <h1 className="text-3xl text-center my-6">Thank You</h1>
+      <h1 className="text-3xl text-center my-6">Хвала на поруци</h1>
       <p className="text-center">
-        Your message has been received. I will get back to you as soon as I can!
+        Примили смо вашу поруку.Јавићемо се ускоро!
       </p>
       <img className="max-w-4xl mx-auto my-6" src="/thank-you.jpeg"></img>
     </Layout>
